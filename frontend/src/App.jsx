0010aaebcc884d422111a2c0b7bff146e7e4f672
James Hyatt <jameshyatt@my.tridenttech.edu>
2023-12-28 18:56:53 -0500

    added components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateContact from './pages/CreateContacts';
+import ShowContact from './pages/Contact';
+import EditContact from './pages/EditContact';
+import DeleteContact from './pages/DeleteContact';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/contacts/create' element={<CreateContact />} />
+      <Route path='/contacts/details/:id' element={<ShowContact />} />
+      <Route path='/contacts/edit/:id' element={<EditContact />} />
+      <Route path='/contacts/delete/:id' element={<DeleteContact />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
